1504c27ae4d7c1fbd8f1cdbe2b5b9bdabb62c875
mc2 <>
2020-10-07 14:58:13 +0900

    更新

--- index.php
+++ index.php
@@ -27,9 +27,14 @@
 
 <section>
 
-<?php ?>
-<?php  if ( have_posts() ) : ?>
-	<?php while ( have_posts() ) : the_post(); ?> 
+<?php
+$args = array(
+	'post_type' => 'post',
+	'posts_per_page' => 3,
+);
+$the_query = new WP_query( $args );
+if ( $the_query->have_posts() ) : ?>
+	<?php while ( $the_query->have_posts() ) : $the_query->the_post(); ?> 
 <div class="list">
 <figure><?php the_post_thumbnail('full'); ?></figure>
 <div class="text">
